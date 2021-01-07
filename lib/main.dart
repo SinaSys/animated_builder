42f2cc9f338ae1ae6849b5608776d0928a01839a
SinaSystem <sinasystem47@gmail.com>
2021-01-07 18:48:36 +0330

    commit AddAnimations

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   final Image starsBackground = Image.asset(
     'assets/images/stars.jpg',
     fit: BoxFit.cover,
     height: double.infinity,
     width: double.infinity,
   );
+
   final Image ufo = Image.asset('assets/images/alien.png');
+
+  AnimationController _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animation = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+    _animation.addListener(() {
+      print(_animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +56,26 @@ class MyHomePage extends StatelessWidget {
       alignment: AlignmentDirectional.center,
       children: <Widget>[
         starsBackground,
-        ClipPath(
-          clipper: const BeamClipper(),
-          child: Container(
-            height: 1000,
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                radius: 1.5,
-                colors: [
-                  Colors.yellow,
-                  Colors.transparent,
-                ],
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (_, __) {
+            return ClipPath(
+              clipper: const BeamClipper(),
+              child: Container(
+                height: 1000,
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    radius: 1.5,
+                    colors: [
+                      Colors.yellow,
+                      Colors.transparent,
+                    ],
+                    stops: [0, _animation.value],
+                  ),
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
         ufo,
       ],
